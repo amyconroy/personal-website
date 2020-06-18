830a1c6adfd98411d72b7c2f4896aa693f40436b
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-18 02:07:05 +0100

    update project descriptions

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,9 +19,11 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne:
-    "I'm Amy, a law graduate who fell in love with the impact technology can have on the public sector. This lead to me deciding to do a MSc Computer Science course, which I am currently completing.",
-  paragraphTwo: '',
-  paragraphThree: '',
+    "I'm Amy, a law graduate passionate about the impact that technology can have on the public sector, in particular the legal field. During my undergraduate degree I became incredibly interested in IT law, and subsequently wrote my dissertation on Article 17 of the GDPR and Machine Learning. This lead to me enrolling in a MSc Computer Science course, which I am currently completing at the University of Bristol. My current research project is to create a system to automatically generate summaries of UKHL judgments using supervised machine learning.",
+  paragraphTwo:
+    "I am experienced with programming languages such as C/C++, Python, Java, Javascript, HTML, and CSS. I am a fast-learner, and am always looking to expand my skill set! When I'm not coding however, I love reading, travelling, and creative writing. I currently have a published novel on Wattpad with nearly 500,000 reads. My favourite pastime has to be figure skating, which I have done for the past 18 years.",
+  paragraphThree:
+    'I am open to, and excited about, new opportunities upon the completion of my MSc degree in September 2020. If you think you have a role that might work well for me, please get in touch!',
   resume: 'AmyConroyCV.pdf', // if no resume, the button will not show up
 };
 
@@ -31,8 +33,10 @@ export const projectsData = [
     id: uuidv1(),
     img: 'project3.jpg',
     title: 'Do or Die Fitness Tracker',
-    info: '',
-    info2: '',
+    info:
+      'Do or Die was created as a group project for our Software Engineering module. It is a wearable fitness tracker, built on a M5Stack. The two other key subsystems include a website built with React where sponsors can create challenges, and users can monitor their data. Our other subsystem is the management dashboard for the Admins of the product, built with Processing.',
+    info2:
+      'I acted as team leader for the project, implementing Agile methods to deliver our working prototype. Our Github page includes our portfolio for the project, which details the design and implementation of the product.',
     url: '',
     repo: 'https://github.com/amyconroy/Do-or-Die-Fitness-Tracker', // if no repo, the button will not show up
   },
@@ -40,7 +44,8 @@ export const projectsData = [
     id: uuidv1(),
     img: 'project2.jpg',
     title: 'PLAY Text Adventure Game Website',
-    info: '',
+    info:
+      "This website for the completely original ‘PLAY text adventure game’ engine, in the style of the retro 80's, was a pair project completed as coursework. Features of the website include a comment page, a fully working shop (with a products and downloads page, persistent basket, and a receipt generated using the database), as well as a demo page, including a demo of the text adventure game written in javascript.",
     info2: '',
     url: '',
     repo: 'https://github.com/amyconroy/play', // if no repo, the button will not show up
@@ -49,8 +54,10 @@ export const projectsData = [
     id: uuidv1(),
     img: 'project4.jpg',
     title: 'Homebrew Database',
-    info: '',
-    info2: '',
+    info:
+      'This is a database system built from the ground up, written in Java. The system allows for all SQL queries to be parsed and executed, for Entity-Relationship data to be stored and manipulated, as well as a socket server to accept incoming connections.',
+    info2:
+      'Developing this system allowed me to apply Object-Oriented programming techniques and design patterns in a practical manner, as well as learning more about database management.',
     url: '',
     repo: 'https://github.com/amyconroy/database-system', // if no repo, the button will not show up
   },
@@ -58,9 +65,11 @@ export const projectsData = [
     id: uuidv1(),
     img: 'project.jpg',
     title: 'Undergraduate Research Project: Article 17 and Machine Learning',
-    info: '',
-    info2: '',
-    url: '../../mock/fyrp.pdf',
+    info:
+      'This is my Final Year Research Project for my Honours Law Degree (LLB) at the University of Bristol. This paper considers whether Article 17 of the GDPR truly provides protection for personal data rights in regards to machine learning algorithms.',
+    info2:
+      'The paper is provides a new take on if the GDPR was written with a consideration for the details of new technologies and their effect on personal data rights. If you would like to discuss this, please get in touch!',
+    url: '/#',
     repo: '', // if no repo, the button will not show up
   },
 ];
